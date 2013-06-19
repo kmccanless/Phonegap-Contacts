6d3e9cb8bc13ff4d11afea91e8a08ad1160a05a0
Keith McCanless <kmccanless@commandalkon.com>
2013-06-19 15:28:09 -0400

    phonegap

---
+++ www/js/caiMobile-controllers.js
@@ -0,0 +1,46 @@
+caiMobile.controller('listCtrl', function ContactListCtrl($scope, $http,$location) {
+    $scope.number = 999;
+    $http.get('http://obscure-caverns-8872.herokuapp.com/contacts').
+        success(function(data, status, headers, config){
+            if(data){
+                $scope.contacts = data.contacts;
+            }
+        });
+    $scope.getContactDetails = function(email) {
+        $location.path( '/contact/' +  email );
+    }
+});
+
+caiMobile.controller('showCtrl', function ContactShowCtrl($scope, $http, $routeParams, $location) {
+    $scope.showController = function() {
+        console.log("clicked");
+        return 'showCtrl';
+    }
+    $http.get('http://obscure-caverns-8872.herokuapp.com/contact/'+$routeParams.id).
+        success(function(data, status, headers, config) {
+            if(data){
+                $scope.contact = data.contact;
+            }else {
+                //handle error/redirect to list
+                $location.path('/');
+            }
+        });
+    $scope.back = function() {
+        $location.path('/');
+    }
+});
+
+caiMobile.controller('addCtrl', function PostAddCtrl($scope, $http, $location) {
+    $scope.submitPost = function() {
+        $http.post('http://obscure-caverns-8872.herokuapp.com/contact', {
+            post_title: $scope.post_title,
+            post_body: $scope.post_body
+        }).success(function(data, status, headers, config) {
+                if(data.success){
+                    $location.path('/');
+                }else {
+                    //do something about the error
+                }
+            });
+    };
+});
